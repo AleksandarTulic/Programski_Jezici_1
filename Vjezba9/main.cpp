7004f1742dab3c6aa865c2f853a8f23a7e3ca227
Aleksandar_Tulic <39736298+AleksandarTulic@users.noreply.github.com>
2019-12-22 20:25:44 +0100

    Update main.cpp

--- Vjezba9/main.cpp
+++ Vjezba9/main.cpp
@@ -3,11 +3,23 @@
 int main()
 {
     niz A(3);
-    add(12, A);
-    add(-34, A);
+    add(123, A);
+    add(45, A);
     add(45, A);
     cout<<A;
     add(456900, A);
     cout<<A;
+
+    niz B(2);
+    add(123, B);
+    add(45, B);
+    add(45, B);
+    add(456900, B);
+
+    if ( A == B ){
+        cout<<"DA";
+    }else{
+        cout<<"NE";
+    }
     return 0;
 }
